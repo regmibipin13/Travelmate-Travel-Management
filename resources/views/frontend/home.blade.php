4d7312452a04a81b0c1b753f5f1a357493a02193
James Munywoki <58624365+lifecode-j@users.noreply.github.com>
2023-04-25 14:37:15 +0545

    All Changes Done

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -37,7 +37,7 @@
     <div class="container-fluid py-5">
         <div class="container pt-5 pb-3">
             <div class="text-center mb-3 pb-3">
-                <h6 class="text-primary text-uppercase" style="letter-spacing: 5px;">Destination</h6>
+                <h2 class="text-primary text-uppercase" style="letter-spacing:5px;">Destination</h2>
                 <h1>Our Top Destination</h1>
             </div>
             <div class="row">
@@ -63,35 +63,36 @@
     <div class="container-fluid py-5">
         <div class="container pt-5 pb-3">
             <div class="text-center mb-3 pb-3">
-                <h6 class="text-primary text-uppercase" style="letter-spacing: 5px;">Services</h6>
+                <h2 class="text-primary text-uppercase" style="letter-spacing: 5px;">Services</h2>
                 <h1>Tours & Travel Services</h1>
             </div>
             <div class="row">
                 <div class="col-lg-4 col-md-6 mb-4">
                     <div class="service-item bg-white text-center mb-2 py-5 px-4">
                         <i class="fa fa-2x fa-route mx-auto mb-4"></i>
-                        <h5 class="mb-2">Travel Guide</h5>
-                        <p class="m-0">Justo sit justo eos amet tempor amet clita amet ipsum eos elitr. Amet lorem
-                            est
-                            amet labore</p>
+                        <h5 class="mb-2">Trekking</h5>
+                        <p class="m-0">Embark on your trekking adventure with a well-planned itinerary,
+                            appropriate gear, and a reliable guide to navigate through the challenges of the terrain,
+                            while embracing the beauty of nature and enjoying the experience to the fullest</p>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6 mb-4">
                     <div class="service-item bg-white text-center mb-2 py-5 px-4">
                         <i class="fa fa-2x fa-ticket-alt mx-auto mb-4"></i>
                         <h5 class="mb-2">Ticket Booking</h5>
-                        <p class="m-0">Justo sit justo eos amet tempor amet clita amet ipsum eos elitr. Amet lorem
-                            est
-                            amet labore</p>
+                        <p class="m-0">Save time and effort by booking your plane or bus tickets online in advance
+                            and choose from a range of options to suit your budget and travel preferences, ensuring a smooth
+                            and stress-free journey ahead."</p>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6 mb-4">
                     <div class="service-item bg-white text-center mb-2 py-5 px-4">
                         <i class="fa fa-2x fa-hotel mx-auto mb-4"></i>
-                        <h5 class="mb-2">Hotel Booking</h5>
-                        <p class="m-0">Justo sit justo eos amet tempor amet clita amet ipsum eos elitr. Amet lorem
-                            est
-                            amet labore</p>
+                        <h5 class="mb-2">Thrill-seeking Adventures Booking</h5>
+                        <p class="m-0">Get ready to push your limits and test your courage with adrenaline-pumping
+                            activities like
+                            bungee jumping, and rafting, as you soar through the skies, dive into the depths,
+                            and ride the rapids of nature's most stunning landscapes</p>
                     </div>
                 </div>
             </div>
@@ -104,7 +105,7 @@
     <div class="container-fluid py-5">
         <div class="container pt-5 pb-3">
             <div class="text-center mb-3 pb-3">
-                <h6 class="text-primary text-uppercase" style="letter-spacing: 5px;">Packages</h6>
+                <h2 class="text-primary text-uppercase" style="letter-spacing: 5px;">Packages</h2>
                 <h1>Pefect Tour and Trek Packages</h1>
             </div>
             <div class="row">
--- resources/views/frontend/service.blade.php
+++ resources/views/frontend/service.blade.php
@@ -20,56 +20,69 @@
     <div class="container-fluid py-5">
         <div class="container pt-5 pb-3">
             <div class="text-center mb-3 pb-3">
-                <h6 class="text-primary text-uppercase" style="letter-spacing: 5px;">Services</h6>
+                <h2 class="text-primary text-uppercase" style="letter-spacing: 5px;">Services</h2>
                 <h1>Tours & Travel Services</h1>
             </div>
             <div class="row">
                 <div class="col-lg-4 col-md-6 mb-4">
                     <div class="service-item bg-white text-center mb-2 py-5 px-4">
                         <i class="fa fa-2x fa-route mx-auto mb-4"></i>
-                        <h5 class="mb-2">Travel Guide</h5>
-                        <p class="m-0">Justo sit justo eos amet tempor amet clita amet ipsum eos elitr. Amet lorem est
-                            amet labore</p>
+                        <h5 class="mb-2">Trekking</h5>
+                        <p class="m-0">Embark on your trekking adventure with a well-planned itinerary, appropriate gear,
+                            and a reliable guide to navigate through the challenges of the terrain, while embracing the
+                            beauty of nature and enjoying the experience to the fullest.</p>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6 mb-4">
                     <div class="service-item bg-white text-center mb-2 py-5 px-4">
                         <i class="fa fa-2x fa-ticket-alt mx-auto mb-4"></i>
                         <h5 class="mb-2">Ticket Booking</h5>
-                        <p class="m-0">Justo sit justo eos amet tempor amet clita amet ipsum eos elitr. Amet lorem est
-                            amet labore</p>
+                        <p class="m-0">Save time and effort by booking your plane or bus tickets online in advance, and
+                            choose from a range of options to suit your budget and travel preferences, ensuring a smooth and
+                            stress-free journey ahead.</p>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6 mb-4">
                     <div class="service-item bg-white text-center mb-2 py-5 px-4">
                         <i class="fa fa-2x fa-hotel mx-auto mb-4"></i>
-                        <h5 class="mb-2">Hotel Booking</h5>
-                        <p class="m-0">Justo sit justo eos amet tempor amet clita amet ipsum eos elitr. Amet lorem est
-                            amet labore</p>
+                        <h5 class="mb-2">Paragliding</h5>
+                        <p class="m-0">Embark on an awe-inspiring adventure as you soar through the skies with the wind
+                            in your face, taking in panoramic views of stunning landscapes below. Whether you're a seasoned
+                            paraglider or a first-timer, our experienced pilots and top-of-the-line equipment ensure a safe
+                            and exhilarating experience that you'll never forget</p>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6 mb-4">
                     <div class="service-item bg-white text-center mb-2 py-5 px-4">
                         <i class="fa fa-2x fa-route mx-auto mb-4"></i>
-                        <h5 class="mb-2">Travel Guide</h5>
-                        <p class="m-0">Justo sit justo eos amet tempor amet clita amet ipsum eos elitr. Amet lorem est
-                            amet labore</p>
+                        <h5 class="mb-2">Bunjee Jump</h5>
+                        <p class="m-0">Challenge yourself and conquer your fears with an adrenaline-pumping bungee jump,
+                            as you take a leap of faith into the unknown and feel the rush of freefalling through the air.
+                            Our expert team ensures your safety and provides the necessary equipment, so you can focus on
+                            the thrill of the moment and make memories that will last a lifetime</p>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6 mb-4">
                     <div class="service-item bg-white text-center mb-2 py-5 px-4">
                         <i class="fa fa-2x fa-ticket-alt mx-auto mb-4"></i>
-                        <h5 class="mb-2">Ticket Booking</h5>
-                        <p class="m-0">Justo sit justo eos amet tempor amet clita amet ipsum eos elitr. Amet lorem est
-                            amet labore</p>
+                        <h5 class="mb-2">Rafting</h5>
+                        <p class="m-0">Embark on an exhilarating adventure as you navigate through rushing rapids,
+                            surrounded by breathtaking scenery and the power of nature. Our experienced guides ensure your
+                            safety and provide the necessary equipment, so you can enjoy the thrill of the ride and the
+                            beauty of your surroundings without worry. Whitewater rafting is a must-do experience for any
+                            thrill-seeker, and we guarantee an unforgettable journey that will leave you feeling alive and
+                            invigorated</p>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6 mb-4">
                     <div class="service-item bg-white text-center mb-2 py-5 px-4">
                         <i class="fa fa-2x fa-hotel mx-auto mb-4"></i>
-                        <h5 class="mb-2">Hotel Booking</h5>
-                        <p class="m-0">Justo sit justo eos amet tempor amet clita amet ipsum eos elitr. Amet lorem est
-                            amet labore</p>
+                        <h5 class="mb-2">Cannoying</h5>
+                        <p class="m-0">Explore the stunning natural beauty of canyons and waterfalls as you rappel down
+                            towering cliffs, slide down chutes, and jump into crystal clear pools. Our expert guides ensure
+                            your safety and provide the necessary equipment, so you can enjoy this thrilling adventure with
+                            confidence. Canyoning is a unique and exciting way to experience the outdoors and create
+                            unforgettable memories that will last a lifetime</p>
                     </div>
                 </div>
             </div>
