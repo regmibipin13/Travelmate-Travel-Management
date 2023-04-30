1817e05749e295d3d7b796270759b207fcb2ffe3
Bipin <regmibipin13@gmail.com>
2023-04-30 13:43:38 +0545

    completed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>{{ env('APP_NAME') }}</title>
+    <title>@yield('site_title', 'Tripways Travels')</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="Free HTML Templates" name="keywords">
     <meta content="Free HTML Templates" name="description">
